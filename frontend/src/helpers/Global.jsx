0f9c7a393a316cda72b68095fe808d3b8321085d
Jonathan12V <2023_jonathan.valle@iticbcn.cat>
2024-03-24 12:39:56 +0100

    modificación algunos archivos

--- frontend/src/helpers/Global.jsx
+++ frontend/src/helpers/Global.jsx
@@ -4,5 +4,5 @@ const apiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3901' 
 export const Global = {
     url: `${apiUrl}/api/user/`,
     urlAdmin: `${apiUrl}/api/admin/`,
-    urlCursos: `${apiUrl}/api/cursos/`
+    urlCursos: `${apiUrl}/api/curso/`
 }
