b40a401125d54ec204d86f9573f7da682b3ee617
Jonathan12V <2023_jonathan.valle@iticbcn.cat>
2024-03-24 03:09:56 +0100

    Modificar rutas para produccion

--- frontend/src/helpers/Global.jsx
+++ frontend/src/helpers/Global.jsx
@@ -1,6 +1,6 @@
 // Urls de la API al que apunta mi NodeJS
 export const Global = {
-    url: 'http://localhost:3900/api/',
-    urlAdmin: 'http://localhost:3900/admin/',
-    urlCursos: 'http://localhost:3900/cursos/'
+    url: 'http://93.176.147.216:3900/api/',
+    urlAdmin: 'http://93.176.147.216:3900/admin/',
+    urlCursos: 'http://93.176.147.216:3900/cursos/'
 }
