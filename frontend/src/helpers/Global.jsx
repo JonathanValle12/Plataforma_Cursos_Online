e6785acf84653a38e1d125ab4ba024466a897c5f
Jonathan12V <2023_jonathan.valle@iticbcn.cat>
2024-04-13 22:54:49 +0200

    cambios variable entorno

--- frontend/src/helpers/Global.jsx
+++ frontend/src/helpers/Global.jsx
@@ -1,5 +1,6 @@
 // Urls de la API al que apunta mi NodeJS
-const apiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3901' : process.env.BACKEND_URL;
+const apiUrl = process.env.VITE_NODE_ENV === 'development' ? 'http://localhost:3901' : process.env.VITE_BACKEND_URL;
+console.log(process.env.VITE_BACKEND_URL);
 
 export const Global = {
     url: `${apiUrl}/api/user/`,
