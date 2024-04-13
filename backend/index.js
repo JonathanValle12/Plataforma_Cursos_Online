d0074cbb0caff0395b8435173a3b703e006ce718
Jonathan12V <2023_jonathan.valle@iticbcn.cat>
2024-04-13 22:28:43 +0200

    añadir opciones de cors

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const corsOptions = {
 };
 
 // Habilitar CORS con opciones personalizadas
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
